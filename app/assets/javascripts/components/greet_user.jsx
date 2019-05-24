c9cd6e36c5d7cd7d7d20d59c843215b18226b5ee
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-24 20:24:19 +0200

    Finish add React components

---
+++ app/assets/javascripts/components/greet_user.jsx
@@ -0,0 +1,7 @@
+window.GreetUser = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello {this.props.name}!</h1>
+    )
+  }
+});
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -16,6 +16,14 @@ class RecordsController < ApplicationController
     @record.destroy
     head :no_content
   end
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
   private
 
   def record_params
